59b73f832f57000033c8a449b1df34ebe4f6ff28
Pedro Marques <pmarques93@gmail.com>
2020-04-12 01:54:35 +0100

    Square Class Creation

--- WolfAndSheep/Program.cs
+++ WolfAndSheep/Program.cs
@@ -1,83 +1,39 @@
 ﻿using System;
 
-namespace testing
+namespace WolfAndSheep
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            // DECLARING VARIABLES
-            int [,] chessboard; 
-            chessboard = new int[8, 8];
+            // Declarar variáveis
+            Square[,] board;
+            board = new Square[8, 8];
 
-            // PRINTS A MESSAGE
-            Console.WriteLine("Wolf Square 1, 3, 5 or 7?");
-
-            // USER INPUT TO GET WOLF SQUARE
-            string aux = Console.ReadLine();
-
-            // CONVERT INPUT TO INT
-            int wolf = Convert.ToInt16(aux);
-
-            // PRINTS CHESSBOARD
-            Console.WriteLine("");
-            Console.Write("    0  1  2  3  4  5  6  7");
-            for (int i = 0; i < chessboard.GetLength(0); i++)
+            for (int i = 0; i < board.GetLength(0); i++)
             {
-                // PRINTS EVERY COLUMN
-                Console.WriteLine("");
-                Console.WriteLine("   -- -- -- -- -- -- -- --");
-                for (int j = 0; j < chessboard.GetLength(1); j++)
+                for (int j = 0; j < board.GetLength(1); j++)
                 {
-                    // EACH TIME THERE IS A NEW ROW
-                    if (j == 0)
-                    {
-                        // WRITES ROW NUMBER
-                        Console.Write($"{i}");
-                        Console.Write($" |");
-
-                        // ONLY FOR THE FIRST COLUMN
-                        if (i % 2 == 0)
-                            Console.Write($"||");
-                        else
-                            Console.Write($"  |");   
-                    } 
-
-                    // SETS WOLF'S INITIAL POSITION ON THE FIRST ROW
-                    else if (i == 0 && j == wolf)
-                        Console.Write("|WW|");
-                    
-                    // SETS SHEEPS INITIAL POSITION ON THE LAST ROW
-                    // if on the last row and j is pair and = 0
-                    else if (i == 7 && j % 2 == 0 || j == 0)
-                        Console.Write("|SS|");
-
-
-                    // PRINTS WHITE SQUARES
-                    // when and j are pair
-                    else if (i % 2 == 0 && j % 2 == 0)
-                        Console.Write($"||");
-                    // WHEN I AND J ARE IMPAIR
-                    else if (i % 2 != 0 && j % 2 != 0)
-                        Console.Write($"||");
-
-                    // PRINTS EMPTY SQUARES
-                    else
-                        Console.Write($"|  |");        
-
+                    board[i,j] = new Square(i, j);
                 }
-                // PRINTS | ON THE LAST COLUMN
-                if (i % 2 != 0)
-                    Console.Write($"|");
-                Console.Write($" {i}");
             }
-            // PRINTS NUMBERS ON THE LAST ROW
-            Console.WriteLine("");
-            Console.WriteLine("   -- -- -- -- -- -- -- --");
-            Console.WriteLine("    0  1  2  3  4  5  6  7");
         }
+     }
 
+    class Square
+    {
+        // Diz se pode mover para o próximo quadrado
+        private static bool isPlayable;
+        // Linha do tabuleiro
+        private static int row;
+        // Coluna do tabuleiro
+        private static int column;
+
+        public Square(int x, int y)
+        {
+            row = x;
+            column = y;
+        }
     }
-
+    
 }
